3af8d4161394697d0bf549dc09e92a565e471f2a
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-07-26 20:13:13 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,22 @@
+$("ul").on("click","li",function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var TodoText =$(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class ='fa fa-trash'></i></span> "+ TodoText+"</li>")
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text'").fadeToggle();
+})
